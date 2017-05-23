89d9e5803cbc3bcdefcddb982b437f6e2fadcb4b
Vasya Kostyk <vuttelvult@gmail.com>
2017-05-23 13:21:13 +0300

    Initial commit

---
+++ src/main/java/vk/springtest/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package vk.springtest.dao;
+
+import vk.springtest.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
